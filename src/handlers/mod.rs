04d67f7d839e68f28aab3efec6e7296f1d86df90
beingglitch <surajshukla5604@gmail.com>
2026-07-28 20:53:17 +0530

    Init Again

--- src/handlers/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod env_handler;
-pub mod key_handler;
-pub mod peer_handler;
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-mod handlers;
-mod cli;
-mod core;
-
-use handlers::key_handler;
-use clap::Parser;
-use handlers::peer_handler;
-
-#[tokio::main]
-async fn main() {
-    let args = cli::Args::parse();
-    match args.command {
-        cli::Commands::Init => key_handler::init(),
-        cli::Commands::Id => key_handler::print_key(),
-        cli::Commands::Status => println!("status"),
-        cli::Commands::Sync { command} => match command {
-            cli::SyncCommands::Pull => core::sync(true).await,
-            cli::SyncCommands::Push => core::sync(false).await
-        },
-        cli::Commands::Peer { command } => match command {
-            cli::PeerCommands::Add { peer_id } => peer_handler::add_peer(peer_id),
-            cli::PeerCommands::Remove { peer_id } => peer_handler::remove_peer(peer_id),
-            cli::PeerCommands::List => peer_handler::list_peers(),
-        }
-    }
+fn main() {
+    println!("Hello, world!");
 }
