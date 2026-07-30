10af8333795bba9f0c34a979f2b28d95059fd78a
beingglitch <surajshukla5604@gmail.com>
2026-07-30 09:29:27 +0530

    feat: Initial Set of CLI commands configured

---
+++ cli/src/main.rs
@@ -0,0 +1,54 @@
+use clap::{Parser, Subcommand};
+use whisper_core;
+
+#[derive(Parser, Debug)]
+struct Args {
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Init,
+    Push,
+    Pull,
+
+    #[command(subcommand)]
+    Peer(PeerCommands),
+}
+
+#[derive(Subcommand, Debug)]
+enum PeerCommands {
+    Add(PeerArgs),
+    Remove(PeerArgs),
+}
+
+#[derive(clap::Args, Debug)]
+struct PeerArgs {
+    peer_id: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        Commands::Init => {
+            whisper_core::init();
+        }
+        Commands::Push => {
+            whisper_core::push_envs();
+        }
+        Commands::Pull => {
+            whisper_core::pull_envs();
+        }
+        Commands::Peer(peer_command) => match peer_command {
+            PeerCommands::Add(PeerArgs { peer_id }) => {
+                whisper_core::add_peer(peer_id);
+            }
+            PeerCommands::Remove(PeerArgs { peer_id }) => {
+                whisper_core::remove_peer(peer_id);
+            }
+        },
+    }
+}
---
+++ core/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn init() {
+
+}
+
+pub fn push_envs() {
+    println!("Pushing env to peers");
+}
+
+pub fn pull_envs() {
+    println!("Pulling env from peers");
+}
+
+pub fn add_peer(peer_id: String) {
+    println!("Adding peer with ID: {}", peer_id);
+}
+
+pub fn remove_peer(peer_id: String) {
+    println!("Removing peer with ID: {}", peer_id);
+}
+
+pub fn search_peer() {
+    println!("Searching for peers");
+}
